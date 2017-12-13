d4e83cb632f0c6574c20bb7570aae8722a669ad1
Mukesh kumar gupta <mkshkmrgpt@gmail.com>
2017-12-13 11:07:07 +0530

    zipkin for spring cloud services

---
+++ log-tracing/src/main/java/com/mkshkmrgpt/logtracing/LogTracingApplication.java
@@ -0,0 +1,14 @@
+package com.mkshkmrgpt.logtracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class LogTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogTracingApplication.class, args);
+	}
+}
